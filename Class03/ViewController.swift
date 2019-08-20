da67b32346d957ae6eff1d4514dfbad10b82fdf3
陳冠伶 <y56142426@gmail.com>
2019-08-20 14:05:41 +0800

    新增專案

---
+++ Class03/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Class03
+//
+//  Created by Linda_Chen on 2019/8/20.
+//  Copyright © 2019 Linda_Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        /* var a = "Hi, Swift."
+           let b = "我愛BTS"
+           a = "change"
+        
+           let c = 123
+           print(a)
+           print(b)
+           print(c)  */
+        
+        var 到超市 = true
+        var 看到包子 = true
+        var 包子數量超過十個 = true
+        var 看到西瓜 = true
+        
+        var 包子的數量 = 0
+        var 西瓜的數量 = 0
+        
+        if 到超市 {
+            
+            if 包子數量超過十個 {
+                
+                包子的數量 = 10
+            
+            } else {
+                包子的數量 = 0
+            }
+            
+            if 看到西瓜 {
+                西瓜的數量 = 1
+            } else {
+                西瓜的數量 = 0
+            }
+        }
+        print(包子的數量)
+        print(西瓜的數量)
+        
+        let result = add(first: 1, second: 2)
+        
+        print(result)
+        
+    }
+
+
+    func add( first : Int, second : Int) -> Int {
+        return 0
+    }
+}
+
